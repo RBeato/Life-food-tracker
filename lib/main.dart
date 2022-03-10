3ce345124887a53a670317c1d1bd1b7b425ecd95
Romeu Beato <romeufbeato@gmail.com>
2022-03-10 17:04:48 +0000

    Added edit page carousel and cards

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,26 @@ void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-//TODO: In the settings the app will allow to choose food from varius types of diets (Paleo, Keto, Wahls...), etc..
+//TODO: Get diets https://www.aconvert.com/document/ods-to-json/ **not completed
+
+//TODO: In the settings the app will allow to choose food from various types of diets (Paleo, Keto, Wahls...), etc..
 //TODO: Allow data export to .xls or other file type
 //TODO: Add safeArea
 //TODO: Add will pop scope
-//TODO: Recover data from .xls if unviolated
+//TODO: Recover data from .xls if unviolated ???
 //TODO: review the way ref.read is used inside build. it shouldn't be used : https://riverpod.dev/docs/concepts/reading/
 //TODO: To create CSV file we are going to use csv 5.0.0 package. PUb DEV
 //TODO: Associate Routine model class items with images
 //TODO: use Hive;
 //TODO: Use https://github.com/openfoodfacts/openfoodfacts-dart/blob/master/DOCUMENTATION.md ??
 //TODO: Possibility to edit the meditation, exercise, symptoms,supplement and medication items lits
-//TODO: Add an alternate meal list with user specified items.
-//TODO: Inserir reset button nas settings para apagar todos os dados e repor as listas de origem
+//TODO: Add an alternate meal list with user specified items. "Your Food List".
+//TODO: Insert reset button in the settings page to erase all data and reset original lists
+//TODO: Add carousels inside the daily_edit_page
+//TODO: set max items selection in the daily_edit_page
+//TODO: set max number of meals, snacks, drinks, and check date time conflits. Add pop-up
+//TODO: set meal ingredients by colors.
+//TODO: add textfield form to edit_page pop up
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
